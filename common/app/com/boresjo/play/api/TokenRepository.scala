07805e2b404724fc135ecf9c980619a953cd02a4
Dan Boresjö <dan@boresjo.com>
2024-11-27 11:17:04 +0000

    Organsed imports

--- common/app/com/boresjo/play/api/TokenRepository.scala
+++ common/app/com/boresjo/play/api/TokenRepository.scala
@@ -1,8 +1,6 @@
 package com.boresjo.play.api
 
-import java.util.concurrent.atomic.AtomicReference
 import scala.concurrent.Future
-import scala.concurrent.Future.successful
 
 trait TokenRepository[A <: AuthToken] {
   def getCurrent(scope: String*): Future[A]
